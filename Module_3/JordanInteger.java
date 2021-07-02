60b0af3e1be82ecb6eef723eb738b37eaff46d26
Ahriika Jordan <ajordan@my365.bellevue.edu>
2021-07-02 09:44:41 -0500

    Progress on the Integer file.

---
+++ Module_3/JordanInteger.java
@@ -0,0 +1,101 @@
+import java.util.function.Function;
+import java.lang.Integer;
+
+public class JordanInteger{
+    public static void main (String args[]) {
+        Integer a = new Integer ("Ahriika");
+        Integer b = new Integer ("Daniel");
+        Integer c = new Integer ("Grace");
+        
+        public int getName(){
+            return Integer;
+            }
+
+        public void setName(){
+            this.a = a;
+        }
+
+        public int isEven(){
+            if (true)
+            return true;
+            else{ 
+                return false;
+            }
+        }
+
+        public int isOdd(){
+            if (true)
+            return true;
+            else{ 
+                return false;
+            }
+        }
+
+        public int isPrime(){
+            if (true)
+            return true;
+            else{ 
+                return false;
+            }
+        }
+
+        final static int isEven(){
+            if(true)
+            return true;
+            else{ 
+                return false;
+            }
+        }
+        
+        final static int isOdd(){
+            if(true)
+            return true;
+            else{ 
+                return false;
+            }
+        }
+
+        final static int isPrime(){
+            if(true)
+            return true;
+            else{ 
+                return false;
+            }
+        }
+
+        final static Integer isEven(){
+            if(true)
+            return true;
+            else{
+                return false;
+            }
+        }
+
+        final static Integer isOdd(){
+            if(true)
+            return true;
+            else{
+                return false;
+            }
+        }
+
+        final static Integer isPrime(){
+            if(true)
+            return true;
+            else{
+                return false;
+            }
+        }
+    }
+
+}
+
+public static void main(String args[]) { 
+    JordanInteger j1 = new JordanInteger(); 
+    JordanInteger j2 = new JordanInteger(true, false, false); 
+    JordanInteger j3 = new JordanInteger(false, true, true); 
+    
+    System.out.println(j1); 
+    System.out.println(j2);
+    System.out.println(j3); 
+}
